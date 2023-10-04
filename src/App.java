28718ecb26977aef9f26b26c98aad212aa8abce8
Te22E-Mrdjen-Damjan <damjan.mrdjen@elev.ga.ntig.se>
2023-10-04 10:47:10 +0200

    uppgift 1 lek5 B) delen

--- src/App.java
+++ src/App.java
@@ -7,8 +7,16 @@ public class App {
 
         System.out.println("fält1 har "+ fält1.length+" antal element (tal)");
 
-        int summa = fält1[0]+fält1[1]+fält1[2]+fält1[3]+fält1[4];
+        int summa1 = fält1[0]+fält1[1]+fält1[2]+fält1[3]+fält1[4];
         System.out.println("Storlek fält "+fält1.length); 
-        System.out.println("medelvärdet är "+summa/5 );
+        System.out.println("medelvärdet är "+summa1/5 );
+        System.out.println("medelvärdet är "+(double)summa1/5 );
+
+        int[] fält2 ={18,14,16,13,19};
+
+        int summa2 = fält2[0]+fält2[1]+fält2[2]+fält2[3]+fält2[4];
+        int summan = summa1+summa2;
+        System.out.println( "summan av fält1 + fält2 är "+summan);
+        System.out.println("medelvärdet är "+ (double)summa2/fält2.length);
     }
 }
